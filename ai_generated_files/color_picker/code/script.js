14697a7c5e01fba689df1c271f8b7074f4af5e07
Mark Kuczmarski <5359580+kuczmama@users.noreply.github.com>
2023-10-11 21:41:40 -0700

    Add color picker

---
+++ ai_generated_files/color_picker/code/script.js
@@ -0,0 +1,3 @@
+// Update the display-color element text content
+var displayColor = document.getElementById("display-color");
+displayColor.textContent = "Color: " + displayColor.textContent.trim();
